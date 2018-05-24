e89377f23258ae1b47ad35c1c323ceb319b450d3
Matthew Kusz <Matt52@roadrunner.com>
2018-05-24 10:30:35 -0400

    Added comments explaining the code more

--- CourseraExcercise3/regLogisticRegression.py
+++ CourseraExcercise3/regLogisticRegression.py
@@ -13,22 +13,28 @@ y_vals = data['y']
 
 
 ####### Regularized logistic regression #######
+
+
 # Sigmoid equation
 def sigmoid(arr_theta, arr_x):
 	return 1.0 / (1.0 + np.exp(-np.dot(arr_x, arr_theta.T)))
 
-# Cost function
-def cost(arr_theta, arr_x, arr_y, lambda1):
+# Regularized cost function
+def reg_cost(arr_theta, arr_x, arr_y, lambda1):
+	# Scipy.optimize.minimize gives array_theta in 1D, so change it back into a 2D array
 	arr_theta = np.reshape(arr_theta, (1, len(arr_theta)))
+	
 	h = sigmoid(arr_theta, arr_x)
 	first_half = np.dot(arr_y.T, np.log(h))
 	second_half = np.dot((1 - arr_y).T, np.log(1 - h))
 	cost = (-1.0 / m) * (first_half + second_half) + (lambda1 / (2 * m)) * (np.dot(arr_theta, arr_theta.T) - arr_theta[0, 0] ** 2)
 	return np.asscalar(cost)
 
-# Cost function gradient
-def cost_gradient(arr_theta, arr_x, arr_y, lambda1):
+# Regularized cost function gradient
+def reg_cost_gradient(arr_theta, arr_x, arr_y, lambda1):
+	# Scipy.optimize.minimize gives array_theta in 1D, so change it back into a 2D array
 	arr_theta = np.reshape(arr_theta, (1, len(arr_theta)))
+
 	h = sigmoid(arr_theta, arr_x)
 	temp = (1.0 / m) * np.dot((h - arr_y).T, arr_x)	
 	gradient = (1.0 / m) * np.add(np.dot((h - arr_y).T, arr_x), (lambda1 / m) * arr_theta)
@@ -38,22 +44,20 @@ def cost_gradient(arr_theta, arr_x, arr_y, lambda1):
 
 
 ####### One-vs-all Classification #######
+
+# Set up what lambda value we want to use
 lambda1 = 1.0
-# Add a column of ones to our vector of x_vals
-m = len(x_vals)   # Number of training examples (rows)
+
+# Add a column of ones to our array of x_vals
+m = len(x_vals)    # Number of training examples (rows)
 arr_ones = np.ones((m, 1))
 x_vals = np.hstack((arr_ones, x_vals))
 
-# Set up our theta vector
+# Set up our theta array
 n = len(x_vals[0])  # Number of columns
 theta_vals = np.zeros((1, n))
-'''
-print x_vals.shape
-print y_vals.shape
-print theta_vals.shape
-'''
 
-# Set up a vector that will be either 1 or 0 depending on which number we are looking at
+# Set up an array that will be either 1 or 0 depending on which number we are looking at
 y_vals_train = np.zeros((len(y_vals), 1))
 for i in range(10):
 	# Set up an array with the values that stand for each number (10 stands for 0)
@@ -67,9 +71,9 @@ for i in range(10):
 			y_vals_train[j] = 0
 	
 	# Use scipys minimize function to compute the theta values
-	minimum = scipy.optimize.minimize(fun = cost, x0 = theta_vals, method = 'CG', jac = cost_gradient, args = (x_vals, y_vals_train, lambda1))#, options = {'disp': True}
+	minimum = scipy.optimize.minimize(fun = reg_cost, x0 = theta_vals, method = 'CG', jac = reg_cost_gradient, args = (x_vals, y_vals_train, lambda1))#, options = {'disp': True}
 	
-	# Create a new vector that consists of all of the new theta values
+	# Create a new array that consists of all of the new theta values
 	theta_new = np.reshape(minimum.x, (1, len(minimum.x)))
 	prob = sigmoid(theta_new, x_vals)
 	prob = np.reshape(prob, (len(prob), 1))
