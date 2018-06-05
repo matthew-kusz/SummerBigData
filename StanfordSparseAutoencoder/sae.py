89fef1e138af0d04b7016b59f0d46bab1748dfcf
Matthew Kusz <Matt52@roadrunner.com>
2018-06-05 14:39:01 -0400

    Preparing for the first test

--- StanfordSparseAutoencoder/sae.py
+++ StanfordSparseAutoencoder/sae.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
 import random
+import math
 
 ####### Definitions #######
 # Sigmoid function
@@ -10,26 +11,30 @@ def sigmoid(value):
 	return 1.0 / (1.0 + np.exp(-value))
 
 # Regularized cost function
-def reg_cost(theta, arr_x, arr_y):	
-	# Reshape our thetas back into their original shape  
-	arr_theta1 = np.reshape(theta[0:25 * n], (25, n))
-	arr_theta2 = np.reshape(theta[25 * n: len(theta)], (10, 26))
+def reg_cost(theta, arr_x):	
+	# Change our weights and bias values back into their original shape
+	arr_W1 = np.reshape(theta[0:global_hidden_size * global_visible_size], (global_hidden_size, global_visible_size))
+	arr_W2 = np.reshape(theta[global_hidden_size * global_visible_size: 2 * global_hidden_size * global_visible_size], (global_visible_size, global_hidden_size))
+	arr_b1 =np.reshape(theta[2 * global_hidden_size * global_visible_size: 2 * global_hidden_size * global_visible_size + global_hidden_size])
+	arr_b2 =np.reshape(theta[2 * hiddenSize * visibleSize+hiddenSize: len(theta)])
+
+	arr_y = arr_x    # (10000, 25) matrix
 
 	# Find our hypothesis
-	h, nano = feedforward(arr_theta1, arr_theta2, arr_x)
+	h, nano = feedforward(arr_W1, arr_W2, arr_b1, arr_b2, arr_x)
 
 	# Find the average activation of each hidden unit averaged over the training set
-	rho_hat = (1.0 / m) * 
+	rho_hat = np.mean(a2, axis = 1)         # (25,) vector
+	rho_hat = np.tile(np.ravel(b1), (m, 1)  # (10000, 25) matrix
 
 	# Calculate the cost
-	first_third = np.multiply(arr_y, np.log(h))
-	second_third = np.multiply((1.0 - arr_y), np.log(1.0 - h))
-	third_third = global_beta * (global_rho * log(global_rho / rho_hat) + (1 - global_rho) * log((1 - global_rho) / (1 - rho_hat)))
-
-	cost1 = np.sum((-1.0 / m) * (first_half + second_half))
-	cost2 = (lambda1 / (2.0 * m)) * (np.sum(arr_theta1 ** 2) - np.sum(arr_theta1[:,0] ** 2))
-	cost3 = (lambda1 / (2.0 * m)) * (np.sum(arr_theta2 ** 2) - np.sum(arr_theta2[:,0] ** 2))
-	cost = cost1 + cost2 + cost3 + third_third
+	KL_divergence = global_beta * (global_rho * log(global_rho / rho_hat) + (1 - global_rho) * log((1 - global_rho) / (1 - rho_hat)))
+	
+	# Calculate the cost
+	cost1 = np.sum((1.0 / (2 * m)) * (h - arr_y) ** 2)
+	cost2 = (global_lambda1 / (2.0 * m)) * (np.sum(arr_W1 ** 2))
+	cost3 = (global_lambda1 / (2.0 * m)) * (np.sum(arr_W2 ** 2))
+	cost = cost1 + cost2 + cost3 + KL_divergence
 	'''
 	# Incase we get booted early
 	if (global_iterations % 20 == 0):
@@ -39,64 +44,118 @@ def reg_cost(theta, arr_x, arr_y):
 	return cost
 
 # Feedforward
-def feedforward(theta1, theta2, arr_x):
-
+def feedforward(W1, W2, b1, b2, arr_x):
 	# We will be running our sigmoid function twice
-	a2 = sigmoid(np.dot(arr_x, theta1.T))	
-
-	# Add a column of ones to our array of a2
-	arr_ones = np.ones((m, 1))
-	a2 = np.hstack((arr_ones, a2))        # (5000, 26) matrix
+	a2 = sigmoid(np.dot(arr_x, W1.T) + np.tile(np.ravel(b1), (m, 1))     # (10000, 25) matrix
 
 	# Second run
-	a3 = sigmoid(np.dot(a2, theta2.T))    # (5000, 10) matrix
+	a3 = sigmoid(np.dot(a2, W2.T) + np.tile(np.ravel(b2), (m, 1)))       # (10000, 64) matrix
 
 	return a3, a2
 
 # Backpropagation
-def backprop(theta, arr_x, arr_y_train):
-	# Change our theta values back into their original shape
-	arr_theta1 = np.reshape(theta[0:global_hidden_size * n], (global_hidden_size, n))
-	arr_theta2 = np.reshape(theta[global_hidden_size * n: len(theta)], (10, 26))
+def backprop(theta, arr_x):
+	# Change our weights and bias values back into their original shape
+	arr_W1 = np.reshape(theta[0:global_hidden_size * global_visible_size], (global_hidden_size, global_visible_size))
+	arr_W2 = np.reshape(theta[global_hidden_size * global_visible_size: 2 * global_hidden_size * global_visible_size], (global_visible_size, global_hidden_size))
+	arr_b1 =np.reshape(theta[2 * global_hidden_size * global_visible_size: 2 * global_hidden_size * global_visible_size + global_hidden_size])
+	arr_b2 =np.reshape(theta[2 * hiddenSize * visibleSize+hiddenSize: len(theta)])
 
-	a3, a2 = feedforward(arr_theta1, arr_theta2, arr_x)
+	arr_y = arr_x
+
+	a3, a2 = feedforward(arr_W1, arr_W2, arr_x)
 	Delta2 = 0
 	Delta1 = 0
 
 	# Find the average activation of each hidden unit averaged over the training set
-	rho_hat = (1.0 / m) * 	
+	rho_hat = np.mean(a2, axis = 1)         # (25,) vector
+	rho_hat = np.tile(np.ravel(b1), (m, 1)  # (10000, 25) matrix
 
+	delta3 = np.multiply(a3 - arr_y, a3 * (1 - a3))                # (10000, 64)
+	delta2 = np.multiply(np.dot(delta3, arr_W2) + global_beta * (-(global_rho / rho_hat) + ((1 - global_rho) / (1 - rho_hat)), a2 * (1 - a2))
+	# delta2 is a (10000, 25) matrix
+	
+	# Compute the partial derivatives
+	pd_W1 = np.dot(delta2.T, arr_x)  # (25, 64)
+	pd_W2 = np.dot(delta3.T, a2)     # (64, 25)
+	pd_b1 = delta2
+	pd_b2 = delta3
+
+	# Computing the batch gradient
+	del_W1 = np.zeros((len(pd_W1), len(pd_W1[0])))
+	del_W2 = np.zeros((len(pd_W2), len(pd_W2[0])))
+	del_b1 = np.zeros((len(pd_b1), len(pd_b1[0])))
+	del_b2 = np.zeros((len(pd_b2), len(pd_b2[0])))
 	for i in range(len(arr_x)):
-		delta3 = a3[i] - arr_y_train[i]                   # Vector length 10
-		
-		delta3 = np.reshape(delta3, (len(delta3), 1))     # (10, 1) matrix
-		temp_a2 = np.reshape(a2[i], (len(a2[i]), 1))      # (26, 1) matrix
-		# Add in sparsity parameter
-		KL_divergence = global_beta * (-(global_rho / rho_hat) + (1 - global_rho) / (1 - rho_hat)
-		delta2 = np.multiply((np.dot(arr_theta2.T, delta3) + KL_divergence), temp_a2 * (1 - temp_a2))
+		del_W1 = del_W1 + pd_W1
+		del_W2 = del_W2 + pd_W2
+		del_b1 = del_b1 + pd_b1
+		del_b2 = del_b2 + pr_b2
 
-		Delta2 = Delta2 + np.dot(delta3, temp_a2.T)       # (10, 26) matrix
+	arr_W1 = arr_W1 - global_alpha * ((1.0 / m) * del_W1 + global_lambda * arr_W1)
+	arr_W2 = arr_W2 - global_alpha * ((1.0 / m) * del_W2 + global_lambda * arr_W2)
+	arr_b1 = arr_b1 - global_alpha * ((1.0 / m) * del_b1 + global_lambda * arr_b1)
+	arr_b2 = arr_b2 - global_alpha * ((1.0 / m) * del_b2 + global_lambda * arr_b2)
+
+	'''
+	for i in range(len(arr_x)):
+		temp_a3 = np.reshape(a3[i], (len(a3[i]), 1))
+		delta3 = np.multiply(a3[i] - arr_y[i], temp_a3 * (1 - temp_a3) # Vector length 64
+		
+		delta3 = np.reshape(delta3, (len(delta3), 1))     # (64, 1) matrix
+		temp_a2 = np.reshape(a2[i], (len(a2[i]), 1))      # (25, 1) matrix
+		temp_rho = np.reshape(rho_hat[i], (lem(rho_hat[i]), 1))   # (25, 1) matrix
+		# Add in sparsity parameter
+		KL_divergence = global_beta * (-(global_rho / rho_hat[i,:] ) + (1 - global_rho) / (1 - rho_hat[i,:]))
+		delta2 = np.multiply((np.dot(arr_W2.T, delta3) + KL_divergence), temp_a2 * (1 - temp_a2))
+
+		Delta2 = Delta2 + np.dot(delta3, temp_a2.T)       # (64, 25) matrix
 		temp_arr_x = np.reshape(arr_x[i], (len(arr_x[i]), 1))
 		
-		# We need to remove delta2[0] from Delta1
-		Delta1 = Delta1 + np.delete(np.dot(delta2, temp_arr_x.T), 0, axis = 0) # (25, 401) matrix
+		Delta1 = Delta1 + np.dot(delta2, temp_arr_x.T)    # (25, 64) matrix
 		
+	
 	# Compute the unregularized gradient
-	D1_temp = (1.0 / m) * Delta1 
-	D2_temp = (1.0 / m) * Delta2
+	b1_grad = (1.0 / m) * arr_b1
+	b2_grad = (1.0 / m) * arr_b2
 	
 	# Compute the regularized gradient
-	D1 = (1.0 / m) * Delta1 + (global_lambda1 / float(m)) * arr_theta1
-	D1[0:, 0:1] = D1_temp[0:, 0:1]    # (25, 401) matrix
-	D2 = (1.0 / m) * Delta2 + (global_lambda1 / float(m)) * arr_theta2
-	D2[0:, 0:1] = D2_temp[0:, 0:1]    # (10, 26) matrix
-
+	D1 = (1.0 / m) * Delta1 + (global_lambda1 / float(m)) * arr_W1 # (25, 64) matrix
+	D2 = (1.0 / m) * Delta2 + (global_lambda1 / float(m)) * arr_W2 # (64, 25) matrix
+	'''
 	# Changed the gradient into a one dimensional vector
 	D1 = np.ravel(D1)
 	D2 = np.ravel(D2)
-	D_vals = np.concatenate((D1, D2))
+	b1_grad = np.ravel(b1_grad)
+	b2_grad = np.ravel(b2_grad)
+	D_vals = np.concatenate((D1, D2, b1_grad, b2_grad))
 
 	return D_vals
+
+# Set up our weights and bias terms
+def weights_bias():
+	# Initialize parameters randomly based on layer sizes.
+	# We'll choose weights uniformly from the interval [-r, r]
+	r  = math.sqrt(6) / math.sqrt(global_hidden_size + global_visible_size + 1);
+	random_weight1 = np.random.rand(global_hidden_size, global_visible_size)     # (25, 64) matrix
+	random_weight1 = random_weight1 * 2 * r - r
+	random_weight2 = np.random.rand(global_visible_size, global_hidden_size)     # (64, 25) matrix      
+	random_weight2 = random_weight2 * 2 * r - r
+
+	# Set up our bias term
+	bias1 = np.zeros((global_hidden_size, 1))     # (25, 1) matrix
+	bias2 = np.zeros((global_visible_size, 1))    # (64, 1) matrix
+
+	# Combine these into a 1-dimension vector
+	random_weight1_1D = np.ravel(random_weight1)
+	bias1_1D = np.ravel(bias1)
+	random_weight2_1D = np.ravel(random_weight2)
+	bias2_1D = np.ravel(bias2)
+
+	# Create a vector theta = W1 + W2 + b1 + b2
+	theta_vals = np.concatenate((random_weight1_1D, random_weight2_1D, bias1_1D, bias2_1D), axis = 1)	
+	
+	return theta_vals
 
 ####### Generate training set #######
 
@@ -138,25 +197,20 @@ plt.imshow(image2, cmap = 'binary', interpolation = 'none')
 ####### Sparse autoencoder objective #######
 global_visible_size = 64
 global_hidden_size = 25
-global_lambda1 = 1
-global_beta = 3
-global_rho = 1              # Sparsity parameter
+global_lambda1 = 0
+global_beta = 0
+global_rho = 0.01              # Sparsity parameter
+global_alpha = 1
+m = len(patches[0])
 
 # We need to add a row of ones onto patches
+arr_ones = np.ones((1, len(patches[0])))
+patches = np.vstack((arr_ones, patches))   # (64, 10000) matrix
+patches = patches.T                        # (10000, 64)
 
-
-# Initialize parameters randomly based on layer sizes.
-r  = sqrt(6) / sqrt(hiddenSize+visibleSize+1);   # We'll choose weights uniformly from the interval [-r, r]
-random_theta1 = np.random.rand(global_hidden_size, global_visible_size)      # (25, 64) matrix
-random_theta1 = random_theta1 * 2 * r - r
-random_theta2 = np.random.rand(global_visible_size + 1, global_hidden_size)      # (65, 25) matrix      
-random_theta2 = random_theta2 * 2 * r - r
-
-# Combine these into a 1-dimension vector
-random_theta1_1D = np.ravel(random_theta1)   
-random_theta2_1D = np.ravel(random_theta2)   
-theta_vals = np.concatenate((random_theta1_1D, random_theta2_1D), axis = 1)
+# Create our weights and bias terms
+theta1 = weights_bias()
 
 # Gradient checking from scipy to see if our backprop function is working properly. Theta_vals needs to be a 1-D vector.
-# print scipy.optimize.check_grad(reg_cost, backprop, theta_vals, x_vals, y_vals_train)
+# print scipy.optimize.check_grad(reg_cost, backprop, theta1, x_vals)
 # Recieved a value of N/A
