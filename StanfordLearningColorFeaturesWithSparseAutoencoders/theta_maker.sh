91e0a1a255b5022466388b3a2f295c0bad26cdb5
Matthew Kusz <Matt52@roadrunner.com>
2018-06-12 16:02:27 -0400

    Running the linear decoder

---
+++ StanfordLearningColorFeaturesWithSparseAutoencoders/theta_maker.sh
@@ -0,0 +1,20 @@
+#PBS -N lcfsaeSize100000L3e-3B5Rho0.035
+#PBS -l walltime=03:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=16GB
+#PBS -j oe
+
+# uncomment if using qsub
+if [ -z "$PBS_O_WORKDIR" ] 
+then
+        echo "PBS_O_WORKDIR not defined"
+else
+        cd $PBS_O_WORKDIR
+        echo $PBS_O_WORKDIR
+fi
+#
+# Setup GPU code
+module load python/2.7.8
+#
+# This is the command the runs the python script
+python -u lcfsae.py >& output_logs/outputLcfsaeSize100000L3e-3B5Rho0.035.log
