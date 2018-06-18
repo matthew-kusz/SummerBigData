5b2a220ade42221f75564c84dddb5f8a15bfc066
Matthew Kusz <Matt52@roadrunner.com>
2018-06-18 17:47:53 -0400

    Adding in convolve

---
+++ StanfordConvolutionAndPooling/cnn_convolve.py
@@ -0,0 +1,25 @@
+# Import the necessary packages
+
+####### Definitions #######
+def convolve(patch_dim, num_features, images, W, b, ZCA_white, mean_patch):
+	# Number of images
+	m = images.shape[3]
+	# Dimension of image
+	img_dim = images.shape[0]
+	# Number of channels
+	num_channel = images.shape[2]
+	print m, img_dim, channel_num
+	
+	convolved_features = np.zeros(num_features, m, img_dim - patch_dim + 1, img_dim - patch_Dim + 1)
+
+	for i in m:
+		for j in num_features:
+		
+		# convolution of image with feature matrix for each channel
+		convolved_image = np.zeros(img_dim - patch_dim + 1, img_dim - patch_dim + 1)
+		for k in num_channel:
+			# Obtain the feature (patch_dim x patch_dim) needed during the convolution
+			feature = np.zeros((patch_dim, patch_dim))
+
+			# Flip the feature matrix because of the definition of convolution, as explained later
+			feature = np.flipud(?)
