dc6983fe33621de18364b7e39154e41b8213544b
Matthew Kusz <Matt52@roadrunner.com>
2018-06-19 09:37:55 -0400

    Continueing work on code

---
+++ StanfordConvolutionAndPooling/run_conv_pool.sh
@@ -0,0 +1,20 @@
+#PBS -N thisisatest
+#PBS -l walltime=00:10:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=16GB
+#PBS -j oe
+
+# uncomment if using qsub
+if [ -z "$PBS_O_WORKDIR" ] 
+then
+        echo "PBS_O_WORKDIR not defined"
+else
+        cd $PBS_O_WORKDIR
+        echo $PBS_O_WORKDIR
+fi
+#
+# Setup GPU code
+module load python/2.7.8
+#
+# This is the command the runs the python script
+python -u cnn_exercise >& output_logs/thisisatest.log
