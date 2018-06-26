609f54f9a7954c41481a6fb420fcb2abdd2105e9
Matthew Kusz <Matt52@roadrunner.com>
2018-06-26 15:05:27 -0400

    Made some adjustments

--- StanfordConvolutionalNeuralNetwork/grab_data.py
+++ StanfordConvolutionalNeuralNetwork/grab_data.py
@@ -3,7 +3,6 @@ import struct as st
 import gzip
 import numpy as np
 import random
-import matplotlib.pyplot as plt
 
 ####### Global variables #######
 global_patch_dim = 15
@@ -25,10 +24,11 @@ def read_idx(filename, n=None):
 def rand_select(images):
 	# Generate a seed so our random values remain the same through each run
 	np.random.seed(7)
+
 	images = np.reshape(images, (len(images), global_image_dim, global_image_dim))
 
 	# Pick our random patches from the MNIST dataset images
-	patches = np.zeros((global_num_patches, global_patch_dim ** 2)) # (10000, 100)
+	patches = np.zeros((global_num_patches, global_patch_dim ** 2)) # (10000, 225)
 	for i in range(global_num_patches):
 		int_random = random.randint(0, global_image_dim - global_patch_dim)
 		pick_image = random.randint(0, len(images) - 1)
@@ -36,14 +36,12 @@ def rand_select(images):
 		temp = image[int_random: int_random + global_patch_dim, int_random: int_random + global_patch_dim]
 		temp = temp.ravel()
 		patches[i:i+1, :] = temp
-	
-	# Flatten the images
-	patches = np.reshape(patches, (len(patches), len(patches[0].ravel())))
+
 	return patches
 
 
 ####### Code #######
-# We need to grab 10000 (10, 10) patches from the MNIST dataset at random to use for our sparse autoencoder
+# We need to grab 10000 (15, 15) patches from the MNIST dataset at random to use for our sparse autoencoder
 
 # Extract the MNIST training data sets
 x_vals = read_idx('provided_data/train-images-idx3-ubyte.gz', 60000)
@@ -53,6 +51,8 @@ print x_vals.shape  # (60000, 784)
 
 # Let's grab the random patches
 rand_patches = rand_select(x_vals)
+
+# Flatten completely to save to file
 rand_patches = rand_patches.ravel()
 
 # Save the file to use for later
--- StanfordConvolutionalNeuralNetwork/theta_maker.sh
+++ StanfordConvolutionalNeuralNetwork/theta_maker.sh
@@ -1,4 +1,4 @@
-#PBS -N finalWeightsMNISTSize10000Patches15x15L0.001B0.5Rho0.05HL100
+#PBS -N finalWeightsMNISTSize10000Patches15x15L0.0001B0.5Rho0.01HL100
 #PBS -l walltime=03:00:00
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=16GB
@@ -17,4 +17,4 @@ fi
 module load python/2.7.8
 #
 # This is the command the runs the python script
-python -u sae.py 0.001 0.05 0.5 >& output_logs/finalWeightsMNISTSize10000Patches15x15L0.001B0.5Rho0.05HL100.log
+python -u sae.py 0.0001 0.01 0.5 >& output_logs/finalWeightsMNISTSize10000Patches15x15L0.0001B0.5Rho0.01HL100.log
