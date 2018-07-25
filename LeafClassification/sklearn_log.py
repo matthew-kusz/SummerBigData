fce797f59b7d80f13123b47b2789894d84bb4b7d
Matthew Kusz <Matt52@roadrunner.com>
2018-07-25 17:25:40 -0400

    sklearn_log.py isn't working right now

--- LeafClassification/sklearn_log.py
+++ LeafClassification/sklearn_log.py
@@ -1,5 +1,9 @@
-from sklearn.model_selection import train_test_split
+import numpy as np				 	 # Allow for easier use of arrays and linear algebra
+import data_setup
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler 	 # Preprocessing
+
+global_max_dim = 50
 
 ####### Code #######
 # Set up the data given to us
@@ -14,20 +18,29 @@ test_mod_list = data_setup.reshape_img(test_list, global_max_dim)
 
 train, test = data_setup.apply_PCA(train, test, train_mod_list, test_mod_list, global_max_dim)
 
-data_setup.more_features(train, test, train_list, test_list)
+# data_setup.more_features(train, test, train_list, test_list)
 
 # fit_transform() calculates the mean and std and also centers and scales data
 x_train = StandardScaler().fit_transform(train)
 x_test = StandardScaler().fit_transform(test)
 
-# test_size: what proportion of original data is used for test set
-train_img, test_img, train_lbl, test_lbl = train_test_split( mnist.data, mnist.target, test_size=1/7.0, random_state=0)
-
 # all parameters not specified are set to their defaults
 # default solver is incredibly slow which is why it was changed to 'lbfgs'
-logisticRegr = LogisticRegression(solver = 'lbfgs')
+log_reg = LogisticRegression(solver = 'lbfgs')
 
-logisticRegr.fit(train_img, train_lbl)
+# FIT IT
+log_reg.fit(x_train, y_train)
 
 # Predict for One Observation (image)
-logisticRegr.predict(test_img[0].reshape(1,-1))
+logisticRegr.predict(x_test)
+
+# Set up the predictions into the correct format to submit to Kaggle
+y_pred = pd.DataFrame(y_pred, index = test_ids, columns = classes)
+
+# Save predictions to a csv file to submit
+if args.save_prob:
+	print 'Saving to file', filename3, '...'
+	fp = open(filename3,'w')
+	fp.write(y_pred.to_csv())
+
+print 'Finished.'
